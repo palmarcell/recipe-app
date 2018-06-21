95aa5ce1353c7389f4ecf607db9c923669b80f6c
palmarcell <palmarcell96@gmail.com>
2018-06-22 01:27:55 +0200

    fixed Category class, added data.sql

--- src/main/java/com/palmarcell/recipeapp/controllers/IndexController.java
+++ src/main/java/com/palmarcell/recipeapp/controllers/IndexController.java
@@ -1,7 +1,16 @@
 package com.palmarcell.recipeapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by gemboly on 2018. 06. 21., 2018
  */
+@Controller
 public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
 }
